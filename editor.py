69faf4e30f3a54e12d8ec8e84030ddef19108637
Tubbles <jae91m@gmail.com>
2024-08-12 08:30:08 +0200

    Add support for xbox 360 controller

--- editor.py
+++ editor.py
@@ -42,6 +42,7 @@ class Game:
     CELLS_HEIGHT = 8
     NUM_CELLS_X = WIDTH // CELLS_WIDTH
     NUM_CELLS_Y = HEIGHT // CELLS_HEIGHT
+    joysticks = {}
     cells = [Cell() for _ in range(NUM_CELLS_X * NUM_CELLS_Y)]
 
     @classmethod
@@ -62,6 +63,7 @@ class Game:
         cls.HEIGHT = cls.screen.get_size()[1]
         cls.CELLS_WIDTH = 8
         cls.CELLS_HEIGHT = 8
+        cls.joysticks = {}
         cls.init_cells()
 
     @classmethod
@@ -144,21 +146,43 @@ def draw():
     Game.screen.blit(Game.font.render(f"fps: {Game.clock.get_fps():.2f}", True, Color.WHITE), (0, 60))
 
 
-def on_key_down(event):
-    if event.key == pygame.K_UP:
+def on_input(input):
+    if input == pygame.K_UP:
         Game.set_rule((Game.get_rule() + 1) % 255)
         Game.up_pressed = True
-    elif event.key == pygame.K_DOWN:
+    elif input == pygame.K_DOWN:
         Game.set_rule((Game.get_rule() - 1) % 255)
         Game.down_pressed = True
-    elif event.key == pygame.K_LEFT:
+    elif input == pygame.K_LEFT:
         Game.CELLS_WIDTH = min(max(1, math.floor(Game.CELLS_WIDTH * 0.8888889)), 250)
         Game.CELLS_HEIGHT = min(max(1, math.floor(Game.CELLS_HEIGHT * 0.8888889)), 250)
         Game.init_cells()
-    elif event.key == pygame.K_RIGHT:
+    elif input == pygame.K_RIGHT:
         Game.CELLS_WIDTH = min(max(1, math.ceil(Game.CELLS_WIDTH * 1.125)), 250)
         Game.CELLS_HEIGHT = min(max(1, math.ceil(Game.CELLS_HEIGHT * 1.125)), 250)
         Game.init_cells()
+
+
+def on_input_release(input):
+    if input == pygame.K_UP:
+        Game.up_pressed = False
+    elif input == pygame.K_DOWN:
+        Game.down_pressed = False
+
+    if not Game.up_pressed and not Game.down_pressed:
+        Game.automate()
+        Game.trigger_full_redraw = True
+
+
+def on_key_down(event):
+    if event.key == pygame.K_UP:
+        on_input(event.key)
+    elif event.key == pygame.K_DOWN:
+        on_input(event.key)
+    elif event.key == pygame.K_LEFT:
+        on_input(event.key)
+    elif event.key == pygame.K_RIGHT:
+        on_input(event.key)
     elif event.key == pygame.K_q and (event.mod == pygame.KMOD_LCTRL or event.mod == pygame.KMOD_RCTRL):
         Game.running = False
     else:
@@ -167,13 +191,61 @@ def on_key_down(event):
 
 def on_key_up(event):
     if event.key == pygame.K_UP:
-        Game.up_pressed = False
+        on_input_release(event.key)
     elif event.key == pygame.K_DOWN:
-        Game.down_pressed = False
+        on_input_release(event.key)
 
-    if not Game.up_pressed and not Game.down_pressed:
-        Game.automate()
-        Game.trigger_full_redraw = True
+
+def on_joy_button_down(event):
+    if "microsoft x-box 360" in Game.joysticks[event.instance_id].get_name().lower():
+        if event.button == 0:  # A Button
+            on_input(pygame.K_DOWN)
+        elif event.button == 1:  # B Button
+            on_input(pygame.K_RIGHT)
+        elif event.button == 2:  # X Button
+            on_input(pygame.K_LEFT)
+        elif event.button == 3:  # Y Button
+            on_input(pygame.K_UP)
+        elif event.button == 4:  # Left Bumper
+            pass
+        elif event.button == 5:  # Right Bumper
+            pass
+        elif event.button == 6:  # Back Button
+            Game.running = False
+        elif event.button == 7:  # Start Button
+            pass
+        elif event.button == 8:  # L. Stick In
+            pass
+        elif event.button == 9:  # R. Stick In
+            pass
+        elif event.button == 10:  # Guide Button
+            pass
+
+
+def on_joy_button_up(event):
+    if "microsoft x-box 360" in Game.joysticks[event.instance_id].get_name().lower():
+        if event.button == 0:  # A Button
+            on_input_release(pygame.K_DOWN)
+        elif event.button == 1:  # B Button
+            on_input_release(pygame.K_RIGHT)
+        elif event.button == 2:  # X Button
+            on_input_release(pygame.K_LEFT)
+        elif event.button == 3:  # Y Button
+            on_input_release(pygame.K_UP)
+        elif event.button == 4:  # Left Bumper
+            pass
+        elif event.button == 5:  # Right Bumper
+            pass
+        elif event.button == 6:  # Back Button
+            pass
+        elif event.button == 7:  # Start Button
+            pass
+        elif event.button == 8:  # L. Stick In
+            pass
+        elif event.button == 9:  # R. Stick In
+            pass
+        elif event.button == 10:  # Guide Button
+            pass
 
 
 def update():
@@ -197,7 +269,6 @@ Game.init()
 Game.set_rule(110)
 Game.automate()
 
-joysticks = {}
 
 while Game.running:
     for event in pygame.event.get():
@@ -215,13 +286,17 @@ while Game.running:
             # This event will be generated when the program starts for every
             # joystick, filling up the list without needing to create them manually.
             joy = pygame.joystick.Joystick(event.device_index)
-            #joy.init()
-            joysticks[joy.get_instance_id()] = joy
-            print(f"Joystick {joy.get_instance_id()} connencted")
+            Game.joysticks[joy.get_instance_id()] = joy
+            print(f"Joystick {joy.get_instance_id()} connected: {joy.get_name()}")
         elif event.type == pygame.JOYDEVICEREMOVED:
-            #joysticks[event.instance_id].quit()
-            del joysticks[event.instance_id]
+            del Game.joysticks[event.instance_id]
             print(f"Joystick {event.instance_id} disconnected")
+        elif event.type == pygame.JOYAXISMOTION:
+            pass
+        elif event.type == pygame.JOYBUTTONDOWN:
+            on_joy_button_down(event)
+        elif event.type == pygame.JOYBUTTONUP:
+            on_joy_button_up(event)
         else:
             print(f"Unknown event: {event}")
 
@@ -233,4 +308,6 @@ while Game.running:
     sys.stdout.flush()
     Game.clock.tick(60)  # limits FPS to 60
 
+# pygame.display.quit()
 pygame.quit()
+sys.exit()
